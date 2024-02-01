af1edf487e1d0c9d5052dbe8ad252feccec6d2c7
Alexandr <94447527+Alexandr-Gluhov@users.noreply.github.com>
2024-02-01 10:25:44 +0300

    Update db.sql

--- third/db.sql
+++ third/db.sql
@@ -1,10 +1,8 @@
 create database if not exists task;
 
-use task;
-
 create table if not exists
 comments (id int primary key auto_increment, comment varchar(255));
 
 create user if not exists php identified by '1234';
 
-grant select, insert on comments to php;
+grant select, insert on task.comments to php;
