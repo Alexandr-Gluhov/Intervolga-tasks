e45d8caa393602a818f955e405c73373f0f5af5e
Alexandr <94447527+Alexandr-Gluhov@users.noreply.github.com>
2024-02-01 08:48:54 +0300

    Update db.sql

--- third/db.sql
+++ third/db.sql
@@ -3,4 +3,4 @@ comments (id int primary key auto_increment, comment varchar(255));
 
 create user if not exists php identified by '1234';
 
-grant select, insert on task.comments to php;
+grant select, insert on comments to php;
